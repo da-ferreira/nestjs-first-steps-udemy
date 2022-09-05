4a9812e5c819b0e8fb704a94448a2097c550a527
da-ferreira <ferreiradavid361@gmail.com>
2022-09-04 21:55:13 -0300

    feat: trabalhando com os parâmetros de uma requisição

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
-  // @Get()
-  @Get('list')
+  @Get()
   findAll() {
     return 'Listagem de cursos';
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return `Curso #${id}`;
   }
 }
