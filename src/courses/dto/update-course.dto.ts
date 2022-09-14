49ff5297d6c4c0117d09ef4477b36728092dd8f0
da-ferreira <ferreiradavid361@gmail.com>
2022-09-14 08:48:38 -0300

    feat: Validando os dados recebidos pelo App - parte 1 Uso de class-validator, PartialTypes e GlobalPipes

--- src/courses/dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto.ts
@@ -1,5 +1,7 @@
-export class UpdateCourseDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly tags?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCourseDto } from './create-course.dto';
+
+// O PartialType faz com que a validação do updateDTO seja parte do que tem
+// no createDTO, ou seja, pode enviar apenas alguns dados do createDTO,
+// mantendo as validações já colocadas
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
