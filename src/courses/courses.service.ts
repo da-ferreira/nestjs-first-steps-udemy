e9acfd2de4a56236ecae06cdf6537ce8f7f5d2dd
da-ferreira <ferreiradavid361@gmail.com>
2022-09-13 22:05:02 -0300

    feat: Tratamento de erros com o NestJS

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Course } from './entities/course.entity';
 
 @Injectable()
@@ -17,7 +17,18 @@ export class CoursesService {
   }
 
   findById(id: string) {
-    return this.courses.find((course: Course) => course.id === Number(id));
+    const course = this.courses.find(
+      (course: Course) => course.id === Number(id),
+    );
+
+    if (!course) {
+      throw new HttpException(
+        `Course id ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return course;
   }
 
   create(createCourseDto: any) {
